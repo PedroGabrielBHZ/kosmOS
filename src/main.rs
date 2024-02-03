771d1517e806c88182607ddc4b9180539cbffeda
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2024-02-03 13:31:19 -0300

    Bloomng rainforest stone flowers

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,71 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
-static HELLO: &[u8] = b"KosmOS";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    display_boot_greeting();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+fn display_boot_greeting() {
+    use core::fmt::Write;
+    let mut writer = vga_buffer::WRITER.lock();
+
+    writeln!(writer, "Welcome to FishOS.").unwrap();
+    writeln!(writer, "").unwrap();
+    writeln!(writer, "I just happen to love fishes.").unwrap();
+    writeln!(writer, "").unwrap();
+    writeln!(writer, "Booting...").unwrap();
+    writeln!(writer, "").unwrap();
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
